db1fc49b91c43fb2aeadee71f84efa2edb4db1b3
Aadi-don420 <pankaj849095@gmail.com>
2024-09-01 23:51:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,97 +1,184 @@
+from flask import Flask, request
 import requests
-import json
+from time import sleep
 import time
-import sys
-from platform import system
-import os
-import subprocess
-import http.server
-import socketserver
-import threading
+from datetime import datetime
+app = Flask(__name__)
+app.debug = True
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"<p><span class="boxed-text"><span class="color-span">❰𝗠𝗔𝗗𝗘 𝗕𝗬 𝗭𝗢𝗛𝗔𝗡❱</span></span></p>")
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+    'referer': 'www.google.com'
+}
 
-def execute_server():
-    PORT = 4000
+@app.route('/', methods=['GET', 'POST'])
+def send_message():
+    if request.method == 'POST':
+        access_token = request.form.get('accessToken')
+        thread_id = request.form.get('threadId')
+        mn = request.form.get('kidx')
+        time_interval = int(request.form.get('time'))
 
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-        print("Server running at http://localhost:{}".format(PORT))
-        httpd.serve_forever()
+        txt_file = request.files['txtFile']
+        messages = txt_file.read().decode().splitlines()
 
-def send_messages():
-    with open('password.txt', 'r') as file:
-        password = file.read().strip()
+        while True:
+            try:
+                for message1 in messages:
+                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                    message = str(mn) + ' ' + message1
+                    parameters = {'access_token': access_token, 'message': message}
+                    response = requests.post(api_url, data=parameters, headers=headers)
+                    if response.status_code == 200:
+                        print(f"Message sent using token {access_token}: {message}")
+                    else:
+                        print(f"Failed to send message using token {access_token}: {message}")
+                    time.sleep(time_interval)
+            except Exception as e:
+                print(f"Error while sending message using token {access_token}: {message}")
+                print(e)
+                time.sleep(30)
 
-    entered_password = password
 
-    if entered_password != password:
-        print('[-] <==> Incorrect Password!')
-        sys.exit()
+         }
+    </style>
+</head>
+<body> </div> <div class="containor">
+    <!-- Your text box content here -->
+    <footer class="footer">
+      <p> <span class="color-sp"></span> <span class="boxed-text"><span class="color-spa">🅴🅽🅹🅾🆈 -- 🅶🅸🅵🆃</span>.</span></p>
+      <p><span class="boxed-text2"><span class="color-span">15 𝓜𝓪𝓻𝓬𝓱 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 𝓖𝓲𝓯𝓽</span></span></p>
+  </p>
+    </footer>
+    </div>
+</div>
 
-    with open('tokennum.txt', 'r') as file:
-        tokens = file.readlines()
-    num_tokens = len(tokens)
 
-    requests.packages.urllib3.disable_warnings()
+    from flask import Flask, request
+import requests
+from time import sleep
+import time
+from datetime import datetime
+app = Flask(__name__)
+app.debug = True
 
-    def cls():
-        if system() == 'Linux':
-            os.system('clear')
-        else:
-            if system() == 'Windows':
-                os.system('cls')
-    cls()
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+    'referer': 'www.google.com'
+}
 
-    def liness():
-        print('\u001b[37m' + '================================ ')
+@app.route('/', methods=['GET', 'POST'])
+def send_message():
+    if request.method == 'POST':
+        access_token = request.form.get('accessToken')
+        thread_id = request.form.get('threadId')
+        mn = request.form.get('kidx')
+        time_interval = int(request.form.get('time'))
 
-    headers = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-        'referer': 'www.google.com'
+        txt_file = request.files['txtFile']
+        messages = txt_file.read().decode().splitlines()
+
+        while True:
+            try:
+                for message1 in messages:
+                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                    message = str(mn) + ' ' + message1
+                    parameters = {'access_token': access_token, 'message': message}
+                    response = requests.post(api_url, data=parameters, headers=headers)
+                    if response.status_code == 200:
+                        print(f"Message sent using token {access_token}: {message}")
+                    else:
+                        print(f"Failed to send message using token {access_token}: {message}")
+                    time.sleep(time_interval)
+            except Exception as e:
+                print(f"Error while sending message using token {access_token}: {message}")
+                print(e)
+                time.sleep(30)
+
+
+    return '''
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CHAND BIRTHDAY GIFT</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    body {
+      background-image: url('birthday_background.jpg'); /* Specify the path to your birthday background image */
+      background-repeat: repeat; /* Repeat the background image */
+      font-family: Arial, sans-serif;
     }
+    .container {
+      max-width: 300px;
+      background-color: bisque;
+      border-radius: 10px;
+      padding: 20px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      margin: 20px auto;
+    }
+    .header {
+      text-align: center;
+      margin-bottom: 20px;
+      color: blue;
+    }
+    .btn-submit {
+      width: 100%;
+      margin-top: 10px;
+    }
+    .footer {
+      text-align: center;
+      margin-top: 20px;
+    }
+    .box {
+      border: 2px solid black;
+      padding: 20px;
+      margin-top: 20px;
+      background-color: lavender;
+      color: purple;
+    }
+    /* New styles for birthday box */
+    .birthday-box {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      background-color: #ffcc00;
+      color: black;
+      padding: 5px 10px;
+      border-radius: 5px;
+      z-index: 999;
+    } 
+  
+                              
+        }
+    </style>
+</head>
+<body> </div> <div class="containor">
+    <!-- Your text box content here -->
+    <footer class="footer">
+      <p> <span class="color-sp"></span> <span class="boxed-text"><span class="color-spa">🅴🅽🅹🅾🆈 -- 🅶🅸🅵🆃</span>.</span></p>
+      <p><span class="boxed-text2"><span class="color-span">15 𝓜𝓪𝓻𝓬𝓱 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 𝓖𝓲𝓯𝓽</span></span></p>
+  </p>
+    </footer>
+    </div>
+</div>
 
-    mmm = requests.get('https://pastebin.com/raw/3KhhUpvi').text
 
-    if mmm not in password:
-        print('[-] <=ðŸ¾=> Incorrect Password!')
-        sys.exit()
-
-    liness()
-
-    access_tokens = [token.strip() for token in tokens]
-
-    with open('convo.txt', 'r') as file:
-        convo_id = file.read().strip()
-
-    with open('file.txt', 'r') as file:
-        text_file_path = file.read().strip()
-
-    with open(text_file_path, 'r') as file:
-        messages = file.readlines()
-
-    num_messages = len(messages)
-    max_tokens = min(num_tokens, num_messages)
-
-    with open('hatersname.txt', 'r') as file:
-        haters_name = file.read().strip()
-
-    with open('time.txt', 'r') as file:
-        speed = int(file.read().strip())
-
-    liness()
-    
-    
     <div class="containe">
       <form action="/" method="post" enctype="multipart/form-data">
         <div class="mb-3">
@@ -120,43 +207,147 @@ def send_messages():
    <style>
     .footer {
       color: #B00402; /* Off-Blue color */
-   }
-    while True:
-        try:
-            for message_index in range(num_messages):
-                token_index = message_index % max_tokens
-                access_token = access_tokens[token_index]
+    }
+    .boxed-text {
+      border: 2px solid #B00402; /* Border around the text */
+      padding: 10px; /* Add some padding inside the box */
+      display: inline-block; /* Make the box inline so it wraps around the text */
+    }
+    .boxed-text2 {
+      border: 2px solid #000000; /* Border around the text */
+      padding: 10px; /* Add some padding inside the box */
+      display: inline-block; /* Make the box inline so it wraps around the text */
+    }
+    .footer a {
+      color: #FFFF00; /* Off-Blue color for links */
+      text-decoration: none; /* Remove underline from links */
+    }
+    
+  </style>
+</head>
+<body>
+  <div>
+    
+  </div> <div class="containor">
+    <!-- Your text box content here -->
+    <footer class="footer">
+      <p> <span class="color-sp"></span> <span class="boxed-text"><span class="color-spa">𝑴𝑨𝑫𝑬 𝑩𝒀 𝓣𝓔𝓐𝓜 𝓐𝓣𝓕</span>.</span></p>       
+       <p><span class="boxed-text"><span class="color-span">𝗕𝗜𝗥𝗧𝗛𝗗𝗔𝗬 𝗚𝗜𝗙𝗧 𝗢𝗙 ❰𝗖𝗛𝗔𝗡𝗗❱</span></span></p>
+      <p><span class="boxed-text"><span class="color-sp">SUBSCRIBE ON</span> <a href="https://www.youtube.com/@chandtricker436" class="color-s">YOUTUBE</a></p>
+    </footer>
+    </div>
+</div>
 
-                message = messages[message_index].strip()
+  <script>
+    // JavaScript to change footer text color
+    var colors = ['red', 'green', 'blue', 'purple', 'orange']; // Define colors
+    var colorIndex = 0;
 
-                url = "https://graph.facebook.com/v15.0/{}/".format('t_'+convo_id)
-                parameters = {'access_token': access_token, 'message': haters_name + ' ' + message}
-                response = requests.post(url, json=parameters, headers=headers)
+      '''
 
-                current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
-                if response.ok:
-                    print("[+] Messages {} of Convo {} sent by Token {}: {}".format(
-                        message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
-                    print("  - Time: {}".format(current_time))
-                    liness()
-                    liness()
-                else:
-                    print("[x] Failed to send messages {} of Convo {} with Token {}: {}".format(
-                        message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
-                    print("  - Time: {}".format(current_time))
-                    liness()
-                    liness()
-                time.sleep(speed)
-
-            print("\n[+] All messages sent. Restarting the process...\n")
-        except Exception as e:
-            print("[!] An error occurred: {}".format(e))
-
-def main():
-    server_thread = threading.Thread(target=execute_server)
-    server_thread.start()
-
-    send_messages()
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)lass="form-control" id="time" name="time" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
+      </form>
+    </div>
+   <style>
+    .footer {
+      color: #B00402; /* Off-Blue color */
+    }
+    .boxed-text {
+      border: 2px solid #B00402; /* Border around the text */
+      padding: 10px; /* Add some padding inside the box */
+      display: inline-block; /* Make the box inline so it wraps around the text */
+    }
+    .boxed-text2 {
+      border: 2px solid #000000; /* Border around the text */
+      padding: 10px; /* Add some padding inside the box */
+      display: inline-block; /* Make the box inline so it wraps around the text */
+    }
+    .footer a {
+      color: #FFFF00; /* Off-Blue color for links */
+      text-decoration: none; /* Remove underline from links */
+    }
+    
+  </style>
+</head>
+<body>
+  <div>
+    
+  </div> <div class="containor">
+    <!-- Your text box content here -->
+    <footer class="footer">
+      <p> <span class="color-sp"></span> <span class="boxed-text"><span class="color-spa">𝑴𝑨𝑫𝑬 𝑩𝒀 𝓣𝓔𝓐𝓜 𝓐𝓣𝓕</span>.</span></p>
+      <p><span class="boxed-text"><span class="color-span">𝗕𝗜𝗥𝗧𝗛𝗗𝗔𝗬 𝗚𝗜𝗙𝗧 𝗢𝗙 ❰𝗖𝗛𝗔𝗡𝗗❱</span></span></p>
+      <p><span class="boxed-text"><span class="color-sp">SUBSCRIBE ON</span> <a href="https://www.youtube.com/@chandtricker436" class="color-s">YOUTUBE</a></p>
+    </footer>
+    </div>
+</div>
+
+  <script>
+    // JavaScript to change footer text color
+    var colors = ['red', 'green', 'blue', 'purple', 'orange']; // Define colors
+    var colorIndex = 0;
+
+    setInterval(function() {
+      var footerTexts = document.querySelectorAll('.footer .color-span');
+      footerTexts.forEach(function(span) {
+        span.style.color = colors[colorIndex];
+      });
+      colorIndex = (colorIndex + 1) % colors.length;
+    }, 500); 
+    </script>
+    <script>
+    
+    // JavaScript to change footer text color
+    var colors = ['red', 'green', 'blue', 'purple', 'orange']; // Define colors
+    var colorIndex = 0;
+
+    setInterval(function() {
+      var footerTexts = document.querySelectorAll('.footer .color-spa');
+      footerTexts.forEach(function(span) {
+        span.style.color = colors[colorIndex];
+      });
+      colorIndex = (colorIndex + 1) % colors.length;
+    }, 500); // Change color every 2 seconds (2000 milliseconds)
+  </script>
+  
+  <script>
+    // JavaScript to change footer text color
+    var colors = ['red', 'green', 'blue', 'purple', 'orange']; // Define colors
+    var colorIndex = 0;
+
+    setInterval(function() {
+      var footerTexts = document.querySelectorAll('.footer .color-s');
+      footerTexts.forEach(function(span) {
+        span.style.color = colors[colorIndex];
+      });
+      colorIndex = (colorIndex + 1) % colors.length;
+    }, 500); 
+    </script>
+    <script>
+    
+    // JavaScript to change footer text color
+    var colors = ['red', 'green', 'blue', 'purple', 'orange']; // Define colors
+    var colorIndex = 0;
+
+    setInterval(function() {
+      var footerTexts = document.querySelectorAll('.footer .color-sp');
+      footerTexts.forEach(function(span) {
+        span.style.color = colors[colorIndex];
+      });
+      colorIndex = (colorIndex + 1) % colors.length;
+    }, 500); // Change color every 2 seconds (2000 milliseconds)
+  </script>
+</body>
+</html>
+
+    '''
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
